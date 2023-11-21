5002d89c2d13020712a0a2279df6d487d4cda944
fkzhao <fkzhao@outlook.com>
2023-11-21 23:11:01 +0800

    first commit

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import TopHeader from "./TopHeader";
+
 import Navbar from "./Navbar";
 import NavigationBar from "@/app/components/header/NavigationBar";
 const Header = () => {
   return (
     <div>
-      {/* Top header*/}
-      {/*<TopHeader />*/}
-      {/* Main header (navbar)*/}
       <NavigationBar />
     </div>
   );
